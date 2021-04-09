6ae03cea888f9a399d34cf1294e9cb2de20f70a8
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-09 14:18:52 -0700

    Add UI  size selection functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-// let smallPizza;
-// let mediumPizza;
-// let largePizza;
-
 function Pizza(size, basePrice) {
   this.size = size
   this.basePrice = basePrice
@@ -14,26 +10,46 @@ Pizza.prototype.calculateTotal = function (toppings) {
 
 
 $(document).ready(function () {
-  let smallPizza = new Pizza("small", 10)
-  let mediumPizza = new Pizza("medium", 15)
-  let largePizza = new Pizza("large", 20)
-  $("form#pizzaOptions").submit(function (event) {
-    event.preventDefault
-    let size = $('#sizeChoice').val()
-    let toppings = parseInt($("form#pizzaOptions").val())
-    if (size === "small") {
-      smallPizza.calculateTotal(toppings)
-      console.log(smallPizza)
-    } else if (size === "medium") {
-      mediumPizza.calculateTotal(toppings)
-    } else if (size === "large") {
-      largePizza.calculateTotal(toppings)
+
+
+  $("form#sizeSelect").submit(function (event) {
+    event.preventDefault();
+    let size = $("#sizeChoice").val();
+    if (size === "1") {
+      pizzaPie = new Pizza("small", 10)
+    }
+    if (size === "2") {
+      pizzaPie = new Pizza("medium", 15)
+    }
+    if (size === "3") {
+      pizzaPie = new Pizza("large", 20)
     }
 
-
-
-
   })
+
+  // $("form#pizzaOptions").submit(function (event) {
+  //   event.preventDefault
+  //   let size = $('#sizeChoice').val()
+  //   let toppings = parseInt($("form#pizzaOptions").val())
+  //   if (size === "small") {
+  //     smallPizza.calculateTotal(toppings)
+  //     console.log(smallPizza)
+  //   } else if (size === "medium") {
+  //     mediumPizza.calculateTotal(toppings)
+  //   } else if (size === "large") {
+  //     largePizza.calculateTotal(toppings)
+  //   }
+  // })
+
+
+
+
+
+
+
+
+
+
 })
 
 
